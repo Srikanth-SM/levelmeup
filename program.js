9c4d21535b7a09e3a87535f61db4856fd40b3f60
Srikanth-SM <srisiro26.pec@gmail.com>
2018-01-17 17:56:12 +0530

    exercise 2

--- program.js
+++ program.js
@@ -1,6 +1,36 @@
-var levelMeUp = function(x, y, callback) {
-    var result = "ALL YOUR " + x + " ARE BELONG TO " + y;
-    callback(null, result);
+// var level = require('level');
+// var levelMeUp = function(x, y, callback) {
+//     var result = "ALL YOUR " + x + " ARE BELONG TO " + y;
+//     callback(null, result);
+// }
+
+
+// end of exercise 1
+
+//exercise 2
+var level = require('level');
+
+var levelMeUp = function(data, key, callback) {
+    var db = level(data);
+    db.get(key, (err, res) => {
+        if (err) {
+            callback(err);
+        }
+        callback(null, res);
+    })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = levelMeUp;
