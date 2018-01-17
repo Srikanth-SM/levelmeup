9b149f204078553c7c3e625d0f0d37171178fe51
Srikanth-SM <srisiro26.pec@gmail.com>
2018-01-17 18:53:28 +0530

    exercise 4

--- program.js
+++ program.js
@@ -20,39 +20,63 @@
 //     })
 // }
 
+// var level = require('level');
+
+// var levelMeUp = function(database, callback) {
+//     var db = level(database);
+//     var error;
+//     var result = [];
+//     db.on('error', (err) => {
+//         error = err;
+//     })
+//     var getData = function(i) {
+//         var key = 'key' + i;
+//         db.get(key, (err, res) => {
+//             if (err) {
+//                 console.log("bang", err);
+//                 // error = err;
+//             } else {
+//                 result.push(res);
+//             }
+//             if (i <= 99 && !error) {
+//                 getData(i + 1);
+//             } else {
+//                 db.close((err) => {
+//                     console.log("error print ", error || err, result);
+//                     callback(null, result)
+//                 })
+//             }
+//         })
+
+//     }
+//     getData(1 - 1);
+
+// }
+
+//exercise 4
 var level = require('level');
 
-var levelMeUp = function(database, callback) {
+var levelMeUp = (database, obj, callback) => {
     var db = level(database);
-    var error;
-    var result = [];
-    db.on('error', (err) => {
-        error = err;
-    })
-    var getData = function(i) {
-        var key = 'key' + i;
-        db.get(key, (err, res) => {
+
+    for (var key in obj) {
+        db.put(key, obj[key], (err, res) => {
+            // console.log(err, res);
             if (err) {
-                console.log("bang", err);
-                // error = err;
-            } else {
-                result.push(res);
-            }
-            if (i <= 99 && !error) {
-                getData(i + 1);
-            } else {
-                db.close((err) => {
-                    console.log("error print ", error || err, result);
-                    callback(null, result)
-                })
+                // console.log("closing error", err)
+                callback(err);
             }
         })
-
     }
-    getData(1 - 1);
-
+    db.close((err) => {
+        if (err) {
+            // console.log("closing error", err)
+            callback(err);
+        } else {
+            callback(null);
+        }
+    })
 }
-
 
 
 
